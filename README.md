1faf7be1eb745716b814ffc9d3edb703524b306a
Thanos <thanosgiap@gmail.com>
2022-03-09 21:15:23 +0200

    Multiple necessary files added

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Erodios - Menu
 
-This project is an under development online restaurant-menu dedicated to restaurant Erodios.
+This project is an under development online restaurant-menu dedicated to restaurant [Erodios](https://www.tripadvisor.com.gr/Restaurant_Review-g667132-d4274886-Reviews-Erodios-Nea_Moudania_Halkidiki_Region_Central_Macedonia.html).
---
+++ src/data.js
@@ -0,0 +1,66 @@
+const menu = [
+    {
+        id: 1,
+        title: 'akara and akamu',
+        category: 'breakfast',
+        price: 1500,
+        desc: `Moin-Moin and Akara are both meals made from pureed beans and spicy ingredients, full of proteins, light and refreshing.`,
+    },
+    {
+        id: 2,
+        title: 'Yam and Egg sauce',
+        category: 'lunch',
+        price: 1700,
+        desc: `Boiled or fried yam and egg sauce is a usual suspect on the tables of Nigerians. It is quite filling and can last you the whole day, if not careful. Egg sauce is an easy-to-prepare Nigerian sauce made by sautéing tomatoes and eggs. `,
+    },
+    {
+        id: 3,
+        title: 'suya',
+        category: 'dinner',
+        price: 1000,
+        desc: `This is simply nicely cut beef that is spiced and grilled over an open flame. It is served with fresh cabbage, tomatoes, and onions.`,
+    },
+    {
+        id: 4,
+        title: 'Tea and bread',
+        category: 'breakfast',
+        price: 1200,
+        desc: `No introductions needed. Children, adults, the elderly, everyone loves Nigerian tea and bread. The tea is made out of cocoa and milk, while the bread can be toasted or eaten with a spread. `,
+    },
+    {
+        id: 5,
+        title: 'jollof rice',
+        category: 'lunch',
+        price: 2500,
+        desc: `No list of Nigerian foods can be complete without a mention of Jollof rice. It's a party dish brought out at celebrations, a signature regional dish, and the recipe with which many West African communities — and countries `,
+    },
+    {
+        id: 6,
+        title: 'ofada rice',
+        category: 'dinner',
+        price: 2200,
+        desc: `This is one of those dishes whose very image inspires appetites from the pickiest of eaters. Be careful though: the dish is pretty spicy and tastes great when paired with tongue-cooling rice.`,
+    },
+    {
+        id: 7,
+        title: 'Indomie and egg',
+        category: 'breakfast',
+        price: 2200,
+        desc: `Indomie is an extremely popular instant noodle brand in Nigeria, so popular that it has become the household name for all instant noodles. `,
+    },
+    {
+        id: 8,
+        title: 'Bread and Beans',
+        category: 'lunch',
+        price: 2500,
+        desc: `Bread and beans or Ewa agoyin is native to the Yoruba people but it is also one of the foods every struggling Nigerian knows well.  `,
+    },
+    {
+        id: 9,
+        title: 'eba and efo riro',
+        category: 'dinner',
+        price: 1150,
+        desc: `Ẹ̀bà is a staple food mainly eaten in the West African subregion, particularly in Nigeria and some parts of Ghana. It came from Nigeria originally`,
+    },
+]
+export default menu
